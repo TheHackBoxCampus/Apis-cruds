cf42f9f3cf5a01596a7ea6b396ea132b9e6d5b8e
TheHackBoxCampus <126725038+TheHackBoxCampus@users.noreply.github.com>
2023-06-21 09:24:52 -0500

    Feat: :tada:  Init project apis

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'spukm01-082/prueba_piloto_miller_kaled',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'spukm01-082/prueba_piloto_miller_kaled' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
